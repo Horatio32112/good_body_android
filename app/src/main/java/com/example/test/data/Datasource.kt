29d2e5cd652260a835d33209828c935cf84c2c69
Horatio <eddiefuh123@gmail.com>
2023-08-01 14:31:16 +0800

    refactor: datasource is simplified. update personal profile and unfollow parts of datasource is refactored

--- app/src/main/java/com/example/test/data/Datasource.kt
+++ app/src/main/java/com/example/test/data/Datasource.kt
@@ -11,6 +11,7 @@ import com.example.test.model.SetsRecord
 import com.example.test.model.TimesRecord
 import com.example.test.model.User
 import com.example.test.model.UserId
+import kotlinx.coroutines.CancellableContinuation
 import kotlinx.coroutines.suspendCancellableCoroutine
 import retrofit2.Call
 import retrofit2.Callback
@@ -67,7 +68,7 @@ class Datasource {
                     if (response.isSuccessful) {
                         try {
                             it.resumeWith(Result.success(response.body()?.user_id))
-                        }catch (ex: ApiException){
+                        } catch (ex: ApiException) {
                         }
 
 
@@ -86,6 +87,7 @@ class Datasource {
             })
         }
     }
+
     @Throws(ApiException::class)
     suspend fun getProfile(account: String): ProfileMessenger {
         return suspendCancellableCoroutine {
@@ -101,7 +103,7 @@ class Datasource {
                         //API回傳結果
 
                         val response = response.body()
-                        val messenger = ProfileMessenger(response,null)
+                        val messenger = ProfileMessenger(response, null)
                         it.resumeWith(Result.success(messenger))
 
                         Log.d("header ", "$account got his own profile")
@@ -122,43 +124,57 @@ class Datasource {
     }
 
     @Throws(ApiException::class)
-    suspend fun updateProfile(account: String,profile: PersonalProfile): PersonalProfile? {
+    suspend fun updateProfile(account: String, profile: PersonalProfile): PersonalProfile {
+        val action: (Response<PersonalProfile>, CancellableContinuation<PersonalProfile>) -> Result<PersonalProfile> = { response, it ->
+            val body = response.body()
+            if (response.isSuccessful&&body!=null) {
+                //API回傳結果
+                Result.success(body)
+            } else {
+                Result.failure(ApiException.Read)
+                // 處理 API 錯誤回應
+            }
+        }
+
+        return callApi({apiBuilder.updateProfile(
+            account,
+            profile.height,
+            profile.weight,
+            profile.age,
+            profile.gender
+        )},action )
+
+
+    }
+
+    private fun <T> callAction(response:Response<T>,it: CancellableContinuation<T>,action: (response:Response<T>,it: CancellableContinuation<T>) -> Result<T>) :Result<T>{
+        return action(response,it)
+    }
+
+    private suspend fun <T> callApi(initApiCaller: () -> Call<T>,parseResponse: (response:Response<T>,it: CancellableContinuation<T>) -> Result<T>): T {
         return suspendCancellableCoroutine {
-            val updateProfileApiCaller = apiBuilder.updateProfile(
-                account,
-                profile.height,
-                profile.weight,
-                profile.age,
-                profile.gender
-            )
-            updateProfileApiCaller.enqueue(object : Callback<PersonalProfile> {
-                override fun onResponse(
-                    call: Call<PersonalProfile>,
-                    response: Response<PersonalProfile>
-                ) {
-                    Log.d("header ", "test ${Thread.currentThread()}")
-
-                    if (response.isSuccessful) {
-                        //API回傳結果
-                        val response = response.body()
-                        it.resumeWith(Result.success(response))
-
-                        Log.d("header ", "$account updated his own profile")
-
-                    } else {
-                        it.resumeWith(Result.failure(ApiException.Read))
-                        Log.d("header ", "$account failed to update his own profile")
-                        // 處理 API 錯誤回應
-                    }
-                }
-
-                override fun onFailure(call: Call<PersonalProfile>, t: Throwable) {
-                    it.resumeWith(Result.failure(ApiException.Call))
-                    Log.d("header ", "$account failed to update his own profile")
-                }
-            })
+            val apiCaller = initApiCaller()
+            enqueueApi(apiCaller,it,parseResponse)
         }
     }
+    private fun <T> enqueueApi(apiCaller: Call<T>, it: CancellableContinuation<T>,parseResponse: (response:Response<T>,it: CancellableContinuation<T>) -> Result<T>) {
+        apiCaller.enqueue(object : Callback<T> {
+            override fun onResponse(
+                call: Call<T>,
+                response: Response<T>
+            ) {
+                Log.d("header ", "test ${Thread.currentThread()}")
+                it.resumeWith(callAction(response,it,parseResponse))
+            }
+
+            override fun onFailure(call: Call<T>, t: Throwable) {
+                Log.d("header ", "Api call failed")
+                it.resumeWith(Result.failure(Exception()))
+            }
+        })
+    }
+
+
     suspend fun loadTimesRecords(account: String): List<TimesRecord> {
         return suspendCancellableCoroutine {
             val retrofitData1 = apiBuilder.getTimesRecords(account)
@@ -298,35 +314,18 @@ class Datasource {
         }
     }
 
-    suspend fun unFollow(SubjectUserAccount: String?, ObjectUserAccount: String): OperationMsg? {
-        return suspendCancellableCoroutine {
-            val retrofitData1 = apiBuilder.unfollow(SubjectUserAccount, ObjectUserAccount)
-            retrofitData1.enqueue(object : Callback<OperationMsg> {
-                override fun onResponse(
-                    call: Call<OperationMsg>,
-                    response: Response<OperationMsg>
-                ) {
-                    Log.d("header ", "test ${Thread.currentThread()}")
+    suspend fun unFollow(SubjectUserAccount: String?, ObjectUserAccount: String): OperationMsg {
 
-                    if (response.isSuccessful) {
-                        //API回傳結果
-                        val response = response.body()
-                        it.resumeWith(Result.success(response))
-
-                        Log.d("header ", "$SubjectUserAccount unfollowed $ObjectUserAccount")
-
-                    } else {
-                        Log.d("header ", "$SubjectUserAccount failed to follow $ObjectUserAccount")
-                        // 處理 API 錯誤回應
-                        it.resumeWith(Result.failure(Exception()))
-                    }
-                }
-
-                override fun onFailure(call: Call<OperationMsg>, t: Throwable) {
-                    Log.d("header ", "FollowApi call failed")
-                    it.resumeWith(Result.failure(Exception()))
-                }
-            })
+        val action: (response:Response<OperationMsg>,it: CancellableContinuation<OperationMsg>) -> Result<OperationMsg> = { response,it ->
+            if (response.isSuccessful) ({
+                //API回傳結果
+                val body = response.body()
+                Result.success(body)
+            }) as Result<OperationMsg> else {
+                Result.failure(ApiException.Read)
+                // 處理 API 錯誤回應
+            }
         }
+        return callApi ({ apiBuilder.unfollow(SubjectUserAccount, ObjectUserAccount)},action)
     }
 }
